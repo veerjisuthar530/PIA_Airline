6b8b90b39a4075edb930337dc23d327faf844917
veerjisuthar530 <veeramdas530@gmail.com>
2025-12-25 13:47:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const tabs = document.querySelectorAll('.booking-tabs button');
+        tabs.forEach(tab => {
+            tab.addEventListener('click', () => {
+                tabs.forEach(t => t.classList.remove('active'));
+                tab.classList.add('active');
+            });
+        });
